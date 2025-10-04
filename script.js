5fd8d696869d627c9bcec309f5063dfa7572390c
ImranSanvi <imranmohammed5220@gmail.com>
2025-10-04 08:08:16 +0600

    font

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// reusable function
+// handleToggle(){
+
+// }
+
+document.getElementById('lesson-1').addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('lesson 1 clicked');
+})
+
+document.getElementById('lesson-2').addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('lesson 2 clicked');
+})

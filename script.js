97aa720d1bd0c92ed05a4d5bee19fbdd95708140
ImranSanvi <imranmohammed5220@gmail.com>
2025-10-05 13:07:31 +0600

    lesson button

--- script.js
+++ script.js
@@ -16,23 +16,39 @@ const loadLevelWord = (id)=>{
 const displayWord = (allWord)=>{
     const wordContainer = document.getElementById('word-card-container')
     wordContainer.innerHTML = ""
-    allWord.forEach(element => {
-        const wordDiv = document.createElement('div')
-        wordDiv.innerHTML = `
-             <div class="card-1 flex flex-col gap-8 bg-white p-5 rounded-[5px] h-[100%] justify-end">
-                <div class="flex flex-col items-center">
-                    <h3 class="font-bold text-[32px]">${element.word}</h3>
-                    <p class="font-medium text-[20px]">Meaning /Pronounciation</p>
-                    <h3 class="font-semibold text-[32px]">${element.meaning} / ${element.pronunciation}</h3>
-                </div>
-                <div class="flex justify-between">
-                    <button class="bg-[#1A91FF]/10 hover:bg-[#1A91FF] w-[56px] h-[56px] rounded-[5px] flex items-center justify-center"><img src="./assets/fa-circle-question.png" alt=""></button>
-                    <button class="bg-[#1A91FF]/10 hover:bg-[#1A91FF] w-[56px] h-[56px] rounded-[5px] flex items-center justify-center"><i class="fa-solid fa-volume-high"></i></button>
-                </div>
+
+    if(allWord.length == 0){
+        wordContainer.innerHTML=`
+            <div class="lesson-7 bg-[#F8F8F8] mx-[80px] flex flex-col gap-3 justify-center items-center py-[80px] col-span-full">
+                <img src="./assets/alert-error.png" alt="">
+                <p class="font-bangla text-[14px]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                <h2 class="font-bangla font-medium text-[35px] text-[#292524]">নেক্সট Lesson এ যান</h2>
             </div>
         `
-        wordContainer.appendChild(wordDiv)
-    });
+    }
+
+    else{
+
+        allWord.forEach(element => {
+            const wordDiv = document.createElement('div')
+            wordDiv.innerHTML = `
+                <div class="card-1 flex flex-col gap-8 bg-white p-5 rounded-[5px] h-[100%] justify-end">
+                    <div class="flex flex-col items-center">
+                        <h3 class="font-bold text-[32px]">${element.word ? element.word : "word not found"}</h3>
+                        <p class="font-medium text-[20px]">Meaning /Pronounciation</p>
+                        <h3 class="font-semibold text-[32px]">${element.meaning ? element.meaning: "meaning not found"} / ${element.pronunciation ? element.pronunciation : "pronunciation not found"}</h3>
+                    </div>
+                    <div class="flex justify-between">
+                        <button class="bg-[#1A91FF]/10 hover:bg-[#1A91FF] w-[56px] h-[56px] rounded-[5px] flex items-center justify-center"><img src="./assets/fa-circle-question.png" alt=""></button>
+                        <button class="bg-[#1A91FF]/10 hover:bg-[#1A91FF] w-[56px] h-[56px] rounded-[5px] flex items-center justify-center"><i class="fa-solid fa-volume-high"></i></button>
+                    </div>
+                </div>
+            `
+            wordContainer.appendChild(wordDiv)
+        });
+    }
+        
+    
 }
 
 const displayLesson = (lesson) => {
@@ -42,7 +58,7 @@ const displayLesson = (lesson) => {
     lesson.forEach(element => {
         const btnDiv = document.createElement('div');
         btnDiv.innerHTML= `
-            <button onclick="loadLevelWord(${element.level_no})" type="button" class="flex justify-center items-center gap-2 border-2 border-[#422AD5] px-2 py-1 rounded-[4px]"><img src="./assets/fa-book-open.png" alt=""><span class="font-semibold text-[14px] text-[#422AD5]">Lesson -${element.level_no}</span></button>
+            <button onclick="loadLevelWord(${element.level_no})" type="button" class="hover:bg-blue-400 flex justify-center items-center gap-2 border-2 border-[#422AD5] px-2 py-1 rounded-[4px]"><img src="./assets/fa-book-open.png" alt=""><span class="font-semibold text-[14px] text-[#422AD5]">Lesson -${element.level_no}</span></button>
         `
         levelContainer.appendChild(btnDiv);
     });
